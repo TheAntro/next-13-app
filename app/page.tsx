7595d3a859d324ccdcfc6e45e57ec2f2cd901e88
Valtteri Ranta <waltherbeach@gmail.com>
2023-07-19 14:37:11 +0300

    Create main nav and some pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
   return <main>{"It's a blank page"}</main>;
 }
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+import styles from './navbar.module.css';
+
 export default function NavBar() {
-  return <nav></nav>;
+  return (
+    <nav className={styles['main-nav']}>
+      <ul className={styles['main-nav__list']}>
+        <li className={styles['main-nav__item']}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles['main-nav__item']}>
+          <Link href="/leagues">Leagues</Link>
+        </li>
+        <li className={styles['main-nav__item']}>
+          <Link href="/tournaments">Tournaments</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
